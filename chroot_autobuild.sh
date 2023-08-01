f29e6d054488449f2960cce87b6460ca4df306ba
Audiodef <audiodef@users.noreply.github.com>
2023-08-01 19:31:53 -0400

    Add files via upload

---
+++ chroot_autobuild.sh
@@ -0,0 +1,241 @@
+# Continued from autobuild.sh
+
+# set -e is !needed here bec it is called by a script outside chroot that uses set -e already.
+
+builddir="/var/tmp/stagebuilder"
+
+### PREP BEFORE PKG INSTALLATION
+
+source /etc/profile
+
+mkdir -p /etc/portage/repos.conf
+cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf
+emerge-webrsync
+
+# Audio config
+cat > /etc/sysctl.conf <<EOF
+vm.swappiness = 10
+dev.rtc.max-user-freq=3072
+dev.hpet.max-user-freq=3072
+EOF
+mkdir -p /etc/security/limits.d
+cat > /etc/security/limits.d/audio.conf <<EOF
+@audio - rtprio 95
+@audio - memlock unlimited
+EOF
+cat > /etc/udev/rules.d/firewire.rules <<EOF
+KERNEL=="fw0", GROUP="audio", MODE="0664"
+KERNEL=="fw1", GROUP="audio", MODE="0664"
+KERNEL=="fw2", GROUP="audio", MODE="0664"
+EOF
+cat > /etc/udev/rules.d/timer-permissions.rules <<EOF
+KERNEL=="rtc0", GROUP="audio"
+KERNEL=="hpet", GROUP="audio"
+EOF
+cat > /etc/udev/rules.d/99-cpu-dma-latency.rules <<EOF
+DEVPATH=="/devices/virtual/misc/cpu_dma_latency", OWNER="root", GROUP="audio", MODE="0660"
+EOF
+
+mkdir -p /usr/lib64/lv2
+ln -s /usr/lib64/lv2/ /usr/lib/lv2 # To be standards-compliant and allow Ardour to scan /usr/lib64/lv2
+
+# Set up repo
+# This should eventually be changed to decibelLinux
+mkdir -p /var/db/repos/decibellinux/metadata/
+cat > /etc/portage/repos.conf/decibelLinux.conf <<EOF
+[GentooStudio]
+location = /var/db/repos/gentoostudio
+sync-type = git
+sync-uri = https://github.com/Gentoo-Music-and-Audio-Technology/gentoostudio.git
+auto-sync = yes
+EOF
+
+# make.conf
+# Temporarily set EMERGE_DEFAULT_OPTS
+cat >> /etc/portage/make.conf <<EOF
+ALSA_CARDS="*"
+EMERGE_DEFAULT_OPTS="--quiet"
+FEATURES="getbinpkg"
+BINPKG_FORMAT="gpkg"
+PORTAGE_BINHOST="https://decibellinux.org/src/binpkgs"
+CONFIG_PROTECT="protect-owned"
+ACCEPT_LICENSE="-* @FREE @BINARY-REDISTRIBUTABLE"
+GRUB_PLATFORMS="efi-64"
+USE="aacplus audacious cddb cdio consolekit corefonts dssi encode equalizer faac ffmpeg fftw fluidsynth freesound
+gudev gtk3 hwdb id3 id3tag ieee1394 jack ladspa lame libsamplerate lv2 matroska midi minizip mpg123 musepack musicbrainz
+netjack opus pcre16 python qt3support quicktime realtime rubberband shine shout skins sndfile soundtouch
+taglib theora timidity twolame vamp vcd vst wav wavpack xine xkb xvfb xvmc -branding -pulseaudio -xscreensaver"
+EOF
+
+cat >> /etc/os-release <<EOF
+NAME=decibelLinux
+ID=decibellinux
+PRETTY_NAME="decibel Linux"
+ANSI_COLOR="1;32"
+HOME_URL="https://www.decbibellinux.org/"
+SUPPORT_URL="https://www.decibellinux.org/"
+BUG_REPORT_URL="https://decibellinux.org/"
+EOF
+
+# package.*
+cd
+wget -r -np -R "index.html*" https://decibellinux.org/src/etc/
+cd decibellinux.org/src/etc/portage
+cp -r * /etc/portage
+cd
+rm -rf decibellinux.org
+
+emerge dev-vcs/git # Needed to sync decibel Linux repo
+emaint sync
+emerge --quiet --update --deep --newuse @world
+eselect news read all # Old news is not relevant to new users
+
+# Install pkgs for decibel Linux, and also build binaries
+emerge --buildpkg --usepkg --buildpkg-exclude "virtual/* sys-kernel/*-sources" \
+app-portage/cpuid2cpuflags \
+app-portage/eix \
+app-portage/genlop \
+app-portage/gentoolkit \
+app-portage/smart-live-rebuild \
+app-portage/ufed \
+dev-util/geany \
+gnome-extra/nm-applet \
+media-plugins/adlplug \
+media-plugins/airwindows \
+media-plugins/alsa-plugins \
+media-plugins/argotlunar \
+media-plugins/artyfx \
+media-plugins/calf \
+media-plugins/cardinal \
+media-plugins/distrho-ports \
+media-plugins/dragonfly-reverb \
+media-plugins/drumgizmo \
+media-plugins/fabla \
+media-plugins/lsp-plugins-lv2 \
+media-plugins/odin \
+media-plugins/opnplug \
+media-plugins/sorcer \
+media-plugins/x42-avldrums \
+media-plugins/x42-plugins \
+media-plugins/zam-plugins \
+media-sound/a2jmidid \
+media-sound/aeolus \
+media-sound/aliki \
+media-sound/alsa-tools \
+media-sound/alsa-utils \
+media-sound/amsynth \
+media-sound/ardour \
+media-sound/arpage \
+media-sound/audacious \
+media-sound/audacity \
+media-sound/bitmeter \
+media-sound/bristol \
+media-sound/butt \
+media-sound/cadence \
+media-sound/carla \
+media-sound/chuck \
+media-sound/din \
+media-sound/fluidsynth \
+media-sound/galan \
+media-sound/ghostess \
+media-sound/gmidimonitor \
+media-sound/hydrogen \
+media-sound/hydrogen-drumkits \
+media-sound/jack-rack \
+media-sound/jack2 \
+media-sound/jackmidiclock \
+media-sound/jamin \
+media-sound/japa \
+media-sound/linuxsampler \
+media-sound/lmms \
+media-sound/luppp \
+media-sound/mixxx \
+media-sound/new-session-manager \
+media-sound/patchage \
+media-sound/pure-data \
+media-sound/qjackctl \
+media-sound/qmidiarp \
+media-sound/qsampler \
+media-sound/qtractor \
+media-sound/rosegarden \
+media-sound/terminatorx \
+media-sound/timemachine \
+media-sound/tk707 \
+media-sound/vmpk \
+media-sound/yoshimi \
+net-misc/dhcpcd \
+net-misc/networkmanager \
+sys-apps/usbutils \
+sys-boot/grub \
+sys-kernel/genkernel \
+sys-kernel/linux-firmware \
+sys-kernel/rt-sources \
+x11-base/xorg-server \
+x11-misc/lightdm \
+x11-misc/mugshot \
+xfce-base/xfce4-meta \
+xfce-extra/xfce4-whiskermenu-plugin \
+xfce-extra/xfce4-alsa-plugin \
+xfce-base/xfce4-power-manager
+
+# Need code to generate list of default installed apps based on above list.
+
+# Config kernel
+# Kernel has to be genkernelled now to generate a .config. Make bzImage only to save time.
+eselect kernel set 1
+genkernel bzImage
+# Fully premptible kernel. Expert is required to select full RT
+sed -i 's/# CONFIG_EXPERT is not set/CONFIG_EXPERT=y/' /usr/src/linux/.config
+sed -i 's/CONFIG_PREEMPT_BUILD=y/CONFIG_PREEMPT_LAZY=y/' /usr/src/linux/.config
+sed -i 's/CONFIG_PREEMPT_VOLUNTARY=y/CONFIG_PREEMPT_RT=y/' /usr/src/linux/.config
+sed -i 's/CONFIG_PREEMPT_DYNAMIC=y/# CONFIG_PREEMPT_DYNAMIC=y/' /usr/src/linux/.config
+# Speculation mitigations security intereferes with real-time operations
+sed -i 's/CONFIG_SPECULATION_MITIGATIONS=y/# CONFIG_SPECULATION_MITIGATIONS is not set/' /usr/src/linux/.config
+# Default CPU freq should be performance
+sed -i 's/# CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE is not set/CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE=y/' /usr/src/linux/.config
+sed -i 's/CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL=y/# CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL is not set/' /usr/src/linux/.config
+# Save a cp of the config for safekeeping.
+cp /usr/src/linux/.config /usr/src/linux/.config_full_preempt
+# Now run genkernel again to make this take effect.
+# genkernel will run a silent make oldconfig, accepting the default changes relevant to config_preempt.
+genkernel --kernel-config=/usr/src/linux/.config_full_preempt all
+# Get rid of old kernels from previous build runs
+rm /boot/*.old
+
+### Enable default services
+systemctl enable lightdm
+systemctl enable NetworkManager
+systemctl enable dhcpcd
+
+### Customize default appearance
+# Current GTK theme/icons is Amy-Dark
+wget https://decibellinux.org/src/xfce4-desktop.xml
+wget https://decibellinux.org/src/xfce4-panel.xml
+wget https://decibellinux.org/src/xsettings.xml
+wget https://decibellinux.org/src/decibelLinux2023.png
+wget https://decibellinux.org/src/Amy-Dark-GTK.tar.gz
+wget https://decibellinux.org/src/Amy-Dark-Icons.tar.gz
+mv xfce4-desktop.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/
+mv xfce4-panel.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/
+mv xsettings.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/
+mv decibelLinux2023.png /usr/share/backgrounds/xfce/
+tar xzf Amy-Dark-GTK.tar.gz
+mv Amy-Dark-GTK /usr/share/themes
+tar xzf Amy-Dark-Icons.tar.gz
+mv Amy-Dark-Icons /usr/share/icons
+rm Amy-Dark-GTK.tar.gz
+rm Amy-Dark-Icons.tar.gz
+
+# Enable all locales and allow user to narrow it down if they choose to.
+# Change this to allow user to select locale.
+cp /usr/share/i18n/SUPPORTED /etc/locale.gen
+locale-gen
+
+# This reduces the tarball size by rm'ing !needed files.
+rm -rf /var/cache/distfiles/*
+rm -rf /usr/src/linux/*
+rm -rf /var/db/repos/* # Will be webrsync'd during install anyway
+rm -rf /var/tmp/*
+
+# Exit chroot
+exit
